3a85f110f00fac5fbb8e2a5b33303d4350cce365
Miguelangel De Armas <dearmasm@wharton.upenn.edu>
2017-08-16 16:53:33 -0700

    Added match.js with moment package

---
+++ backend/match.js
@@ -0,0 +1,51 @@
+import _ from 'underscore';
+import moment from 'moment';
+
+let matchTime = null;
+// let date = new Date();
+// let presentDay = switch (date.getDay()) {
+//   case 0:
+//     day = "Sunday";
+//     break;
+//   case 1:
+//     day = "Monday";
+//     break;
+//   case 2:
+//     day = "Tuesday";
+//     break;
+//   case 3:
+//     day = "Wednesday";
+//     break;
+//   case 4:
+//     day = "Thursday";
+//     break;
+//   case 5:
+//     day = "Friday";
+//     break;
+//   case 6:
+//     day = "Saturday";
+// }
+// let current_hour = date.getHours();
+
+let presentDay = moment().format('dddd'); // Wednesday
+let presentTime = moment().format('LT'); // Local Time
+
+// All times are as of 7:00 AM ET
+const weekly = new Date
+
+let userIds = ['dlkfj', "asdf",]
+
+var userModel = {
+  userId: String;
+  profile: {
+    aslkdfjj
+  },
+  facebookAuth: {
+
+  },
+  interests: [],
+  userMatched: [],
+  whenToMatch: [weekly, daily, search]
+}
+
+if ( presentTime === matchTime )
