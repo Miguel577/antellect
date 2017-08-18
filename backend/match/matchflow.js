2d58683c48d29ea068f8adcd67b1a42d6ff58582
Miguelangel De Armas <dearmasm@wharton.upenn.edu>
2017-08-18 10:40:03 -0700

    Removed extra comments

--- backend/match/matchflow.js
+++ backend/match/matchflow.js
@@ -3,31 +3,6 @@ import moment from 'moment';
 import matchNow from './matchNowFun';
 import matchLater from './matchLaterFun';
 
-// let date = new Date();
-// let presentDay = switch (date.getDay()) {
-//   case 0:
-//     day = "Sunday";
-//     break;
-//   case 1:
-//     day = "Monday";
-//     break;
-//   case 2:
-//     day = "Tuesday";
-//     break;
-//   case 3:
-//     day = "Wednesday";
-//     break;
-//   case 4:
-//     day = "Thursday";
-//     break;
-//   case 5:
-//     day = "Friday";
-//     break;
-//   case 6:
-//     day = "Saturday";
-// }
-// let current_hour = date.getHours();
-
 // Get current Day and Time
 let presentDay = moment().format('dddd'); // Wednesday
 let presentTime = moment().format('LT'); // Local Time, 4:59 PM
